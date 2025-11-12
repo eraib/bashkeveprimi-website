44fa19010d600431cc5927763d19091beb03641d
Clira <117774775+Clirim99@users.noreply.github.com>
2025-11-13 00:05:47 +0100

    feat: implement footer UI according to figma design

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import './index.css' 
 
 function App() {
-	return <><Header/></>;
+	return 
+	<>
+	<Header/>
+	 <main className="min-h-screen">
+        <h1 className="text-center text-2xl mt-10">Welcome to the OBV!</h1>
+      </main>
+	<Footer/>
+	</>;
 }
 
 export default App;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,29 @@
-const Footer = () => {
-	return <></>;
+import logo from "../assets/icons/logo.svg"
+
+export const Footer = () => {
+	return (
+		<footer className="bg-gray-900 text-white">
+			<div className="flex  sm:px-12 px-4 bg-[#ffffff19] py-7">
+				{/* You can add footer content here later */}
+				<div className="flex-1">
+					<div className="flex">
+						<img src={logo} alt="" />
+						<p className="text-[#ffffff] break-normal max-w-40 text-base ">Organizata Bashkeveprimi</p>
+					</div>
+					<div>
+						<p>Phone: +38345 111 222</p><br/>
+						<p>Address: Kosove</p>
+						<p>60000, Gjilan</p>
+					</div>
+				</div>
+				<div className="flex-1">
+					<p>About Us</p><br/>
+					<p>Contact Us</p>
+				</div>
+			</div>
+			
+		</footer>
+	);
 };
 
 export default Footer;
