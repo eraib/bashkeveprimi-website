4babb293872b0f650c8299c0b4f8f19564f37481
eraib <ibrahimiera302@gmail.com>
2025-11-16 15:04:56 -0100

    refactor: streamline code formatting in App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,20 @@ import BashkeveprimiFeatures from "./components/BashkeveprimiFeatures";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import PeriodicActions from "./components/PeriodicActions";
-import './index.css' 
+import "./index.css";
 
 function App() {
 	return (
-	<>
-	<Header/>
-	 <main className="min-h-screen mt-1">
-		<PeriodicActions/>
-		<BashkeveprimiFeatures/>
-      </main>
-	  
-	<Footer/>
-	</>);
+		<>
+			<Header />
+			<main className="min-h-screen mt-1">
+				<PeriodicActions />
+				<BashkeveprimiFeatures />
+			</main>
+
+			<Footer />
+		</>
+	);
 }
 
 export default App;
