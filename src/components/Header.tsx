12b5e08f90b0bdc984b043d82caae389b476e298
eraib <ibrahimiera302@gmail.com>
2025-11-16 14:34:41 -0100

    refactor: extract logo and company name into Header; clean up the Navbar component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,22 @@
 import Navbar from "./Navbar";
+import logo from "../assets/icons/logo.svg";
 
 const Header = () => {
 	const menuitems = ["Home", "Orphans", "Projects", "Requests"];
 
-	return <Navbar menuitems={menuitems} />;
+	return (
+		<div className="bg-[#F3F2E7] py-4">
+			<div className="h-16 flex items-center justify-between px-4">
+				<div className="text-3xl text-white font-bold flex items-center gap-1.5">
+					<img src={logo} alt="" />
+					<p className="text-[#00CFD0] break-normal max-w-40 text-base ">
+						Organizata Bashkeveprimi
+					</p>
+				</div>
+				<Navbar menuitems={menuitems} />
+			</div>
+		</div>
+	);
 };
 
 export default Header;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,3 @@
-import logo from "../assets/icons/logo.svg";
-
 type NavbarProps = {
 	menuitems: string[];
 };
@@ -14,32 +12,21 @@ const Navbar = ({ menuitems }: NavbarProps) => {
 	}
 
 	return (
-		<nav className="bg-[#F3F2E7] py-4">
-			<div className="h-16 flex items-center justify-between px-4">
-				<div className="text-3xl text-white font-bold flex items-center">
-					<img src={logo} alt="" />
-					<p className="text-[#00CFD0] break-normal max-w-40 text-base ">
-						Organizata Bashkeveprimi
-					</p>
-				</div>
-
-				<div className="flex gap-6 bg-[#F3F2E7] items-center">
-					{menuitems.map((menuitem) => (
-						<a
-							href="#"
-							className="text-black-100 opacity-50 text-base hover:text-white">
-							{menuitem}
-						</a>
-					))}
-					<a
-						href="#"
-						className="text-black-100 opacity-50 text-base hover:text-white "
-						onClick={moveToFooterSmoothly}>
-						About Us
-					</a>
-				</div>
-			</div>
-		</nav>
+		<div className="flex gap-6 bg-[#F3F2E7] items-center">
+			{menuitems.map((menuitem) => (
+				<a
+					href="#"
+					className="text-black-100 opacity-50 text-base hover:text-white">
+					{menuitem}
+				</a>
+			))}
+			<a
+				href="#"
+				className="text-black-100 opacity-50 text-base hover:text-white "
+				onClick={moveToFooterSmoothly}>
+				About Us
+			</a>
+		</div>
 	);
 };
 export default Navbar;
