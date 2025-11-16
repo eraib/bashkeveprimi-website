5b633e961c92ecca6be7202c675ca584be943df5
eraib <ibrahimiera302@gmail.com>
2025-11-16 15:03:25 -0100

    feat: install cslx for conditional tailwind; add hooks folder; add TODO

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { clsx } from "clsx";
+
 interface MenuItem {
 	title: string;
 	action?: (e: React.MouseEvent<HTMLAnchorElement>) => void;
@@ -7,13 +9,19 @@ type NavbarProps = {
 };
 
 const Navbar = ({ menuitems }: NavbarProps) => {
+	// TODO: implement routering to display border bottom on active the menuitem
+	// const { isActiveTab } = useIsActiveTab();
+
 	return (
 		<div className="flex gap-6 bg-[#F3F2E7] items-center">
 			{menuitems.map((menuitem) => (
 				<a
 					href="#"
 					onClick={(e) => menuitem?.action?.(e)}
-					className="text-black-100 opacity-50 text-base hover:text-white border-b">
+					className={clsx(
+						"text-black-100 opacity-50 text-base hover:text-white"
+						// isActiveTab ? " border-b" : ""
+					)}>
 					{menuitem.title}
 				</a>
 			))}
